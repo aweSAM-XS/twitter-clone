900ba94c2de14fe15803bdc22d933bf6e413ed25
Samuel Kiprop <sam.kiprop23@gmail.com>
2023-04-22 00:22:31 +0300

    Fix typos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let names = document.querySelectorAll('.name');
 let usernames = document.querySelectorAll('.username');
-let timeline = document.querySelector('.timeline')
+let timeline = document.querySelector('.timeline');
 
 const getUser = async () => {
     let user = await fetch('https://jsonplaceholder.typicode.com/users/7').then(
@@ -16,38 +16,36 @@ const getUser = async () => {
 };
 
 const createPosts = async () => {
-    let posts = await fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((response) => response.json())
-		.then((response) => response.filter((post) => post['userId'] == 7));
-	let user = await getUser()
-	posts.forEach((post) => {
-		console.log(post)
+	let posts = await fetch('https://jsonplaceholder.typicode.com/posts')
+	.then((response) => response.json())
+	.then((response) => response.filter((post) => post['userId'] == 7));
+    let user = await getUser();
+    posts.forEach((post) => {
 		let postDiv = document.createElement('div');
-		postDiv.className = 'posts';
-		postDiv.innerHTML = `
+        console.log(post);
+        postDiv.className = 'posts';
+        postDiv.innerHTML = `
 		<img
 		src="https://fastly.picsum.photos/id/661/400/400.jpg?grayscale&hmac=RhK4QJh-BHiZAsrf7hplXmymJfTySLlOELe_LTilu44"
 		alt="profile-picture"
 		class="profile-img" />
+		<div class="post-content">
 	<div class="username-container">
 		<h4 class="name">${user.name}</h4>
 		<p class="username">@${user.username}</p>
 		<p>&nbsp; &#183; &nbsp; Mar 21</p>
 	</div>
-	<div class="post-content>
 		<p class='post-title'>
-			${post["title"]}
+			${post.title}
 		</p>
 		<p class='post-content'>
 			aaa ${post.body}
 		</p>
 	</div>
-		`
-		console.log(postDiv)
-		timeline.appendChild(postDiv)
+		`;
+        timeline.appendChild(postDiv);
     });
 };
-
 
 getUser();
 createPosts();
