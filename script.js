5778d770569b4b23e0aa0bac18ec36fc316331f0
Samuel Kiprop <sam.kiprop23@gmail.com>
2023-04-21 23:56:35 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+let names = document.querySelectorAll('.name');
+let usernames = document.querySelectorAll('.username');
+let timeline = document.querySelector('.timeline')
+
+const getUser = async () => {
+    let user = await fetch('https://jsonplaceholder.typicode.com/users/7').then(
+        (response) => response.json()
+    );
+    names.forEach((name) => {
+        name.innerText = user.name;
+    });
+    usernames.forEach((username) => {
+        username.innerText = `@ ${user.username}`;
+    });
+    return user;
+};
+
+const createPosts = async () => {
+    let posts = await fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+		.then((response) => response.filter((post) => post['userId'] == 7));
+	let user = await getUser()
+	posts.forEach((post) => {
+		console.log(post)
+		let postDiv = document.createElement('div');
+		postDiv.className = 'posts';
+		postDiv.innerHTML = `
+		<img
+		src="https://fastly.picsum.photos/id/661/400/400.jpg?grayscale&hmac=RhK4QJh-BHiZAsrf7hplXmymJfTySLlOELe_LTilu44"
+		alt="profile-picture"
+		class="profile-img" />
+	<div class="username-container">
+		<h4 class="name">${user.name}</h4>
+		<p class="username">@${user.username}</p>
+		<p>&nbsp; &#183; &nbsp; Mar 21</p>
+	</div>
+	<div class="post-content>
+		<p class='post-title'>
+			${post["title"]}
+		</p>
+		<p class='post-content'>
+			aaa ${post.body}
+		</p>
+	</div>
+		`
+		console.log(postDiv)
+		timeline.appendChild(postDiv)
+    });
+};
+
+
+getUser();
+createPosts();
